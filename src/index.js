1bf143e3970c34a60a787e9c4830c13282aa14cf
Tati-Zhurr <zhtv88@gmail.com>
2022-08-02 19:34:22 +0200

    feet:index.js

--- src/index.js
+++ src/index.js
@@ -38,8 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let fromArr = Object.keys(MORSE_TABLE);
+    let toArr = Object.values(MORSE_TABLE);
+    let arrResult =[];
+    let letterString;
+    let wordsArr = expr.split('**********');
+    let newString ='';
+    for(let i=0; i < wordsArr.length; i++){
+        if (i!==0){
+            arrResult.push(' ');
+        };
+    let k=0;
+    while (k<=wordsArr[i].length){
+     letterString='';
+     letterString=wordsArr[i].slice(k,k+10);
+     console.log( letterString);
+     newString =''
+     for (let n=0; n<=10; n+=2){
+        if (letterString.slice(n,n+2)==='10'){
+            newString=newString +'.';
+        };
+        if (letterString.slice(n,n+2)==='11'){
+            newString = newString +'-';
+        };
+     }
+     arrResult.push(toArr[fromArr.indexOf(newString)]);
+     k+=10;
+    }
+   
+         
+    }
+    return arrResult.join('');
+
 }
+
+console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'));
 
 module.exports = {
     decode
